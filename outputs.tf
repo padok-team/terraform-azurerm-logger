df37232c62950a94aa740b2e7bd9fe62465c5884
Benjamin Sanvoisin <benjamin@sanvoisin.io>
2022-04-29 16:45:06 +0200

    fix(output): fix output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "azurerm_log_analytics_workspace_id" {
   description = "The workspace ID"
-  value       = try(var.log_analytics_workspace_id,azurerm_log_analytics_workspace.this[0].id)
+  value       = var.log_analytics_workspace_id == null ? azurerm_log_analytics_workspace.this[0].id : var.log_analytics_workspace_id
 }
